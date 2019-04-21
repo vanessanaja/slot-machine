eac1d53b4fbdedc43562ca310d47fd71112bb051
vanessanaja <vanessacnaja@gmail.com>
2019-04-21 15:27:09 -0700

    add styles & updated README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is a small project from Colt Steele's "Modern React Bootcamp" which you can find on Udemy.
 
 www.udemy.com/modern-react-bootcamp
+
+One of the things we learned in this project are different ways to style components. The styles are really not designed to actually look good on this project, just to learn :)
--- machine.js
+++ machine.js
@@ -2,10 +2,13 @@ class Machine extends React.Component {
   render(){
     const {slot1, slot2, slot3} = this.props;
     const winner = (slot1 === slot2) && (slot2 === slot3);
+    const emojiStyle = {fontSize: '50px', backgroundColor: 'blue'}
     return (
-      <div>
-        <p>{slot1} {slot2} {slot3}</p>
-        <p>{winner ? 'Congrats, you won!' : "Sorry, You didn't win"}</p>
+      <div className="Machine">
+        <p style={emojiStyle}>{slot1} {slot2} {slot3}</p>
+        <p className={winner ? 'winner' : 'loser'}>
+        {winner ? 'Congrats, you won!' : "Sorry, You didn't win"}
+        </p>
       </div>
     )
   }
